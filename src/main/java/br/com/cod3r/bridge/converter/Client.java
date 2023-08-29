3e87af039e6cec66495b2dcbfd7cf7bc86d65ae1
Leônidas Moritz Rosa <leogui.moritz@gmail.com>
2023-08-28 22:21:24 -0300

    Implementado padrão Bridge para converter dados de employee para os formatos CSV e Json, possibilitando estender para novos formatos futuramente.

--- src/main/java/br/com/cod3r/bridge/converter/Client.java
+++ src/main/java/br/com/cod3r/bridge/converter/Client.java
@@ -1,8 +1,29 @@
 package br.com.cod3r.bridge.converter;
 
+import br.com.cod3r.bridge.converter.converters.CSVConverter;
+import br.com.cod3r.bridge.converter.converters.Converter;
+import br.com.cod3r.bridge.converter.converters.JsonConverter;
+import br.com.cod3r.bridge.converter.employees.Employee;
+import br.com.cod3r.bridge.converter.employees.ITGuy;
+import br.com.cod3r.bridge.converter.employees.ProjectManager;
+
 public class Client {
 
 	public static void main(String[] args) {
-		
+		Converter csvConverter = new CSVConverter();
+		Converter jsonCoverter = new JsonConverter();
+
+		Employee itGuy = new ITGuy("John Brown", 28, 5800.00);
+		Employee manager = new ProjectManager("Bob Lincoln", 38, 18500.00);
+
+		String itGuyCSVFormatted = csvConverter.getEmployeeFormated(itGuy);
+		String managerCSVFormatted = csvConverter.getEmployeeFormated(manager);
+		String itGuyJsonFormatted = jsonCoverter.getEmployeeFormated(itGuy);
+		String managerJsonFormatted = jsonCoverter.getEmployeeFormated(manager);
+
+		System.out.println(itGuyCSVFormatted);
+		System.out.println(managerCSVFormatted);
+		System.out.println(itGuyJsonFormatted);
+		System.out.println(managerJsonFormatted);
 	}
 }
--- src/main/java/br/com/cod3r/bridge/converter/converters/JsonConverter.java
+++ src/main/java/br/com/cod3r/bridge/converter/converters/JsonConverter.java
@@ -2,15 +2,16 @@ package br.com.cod3r.bridge.converter.converters;
 
 import br.com.cod3r.bridge.converter.employees.Employee;
 
-public class JsonConverter {
+public class JsonConverter implements Converter {
 
+	@Override
 	public String getEmployeeFormated(Employee emp) {
 		StringBuilder builder = new StringBuilder();
 		builder.append("{\n")
-			.append("\t\"name\": \"").append(emp.getName()).append("\"\n")
-			.append("\t\"age\": \"").append(emp.getAge()).append("\"\n")
+			.append("\t\"name\": \"").append(emp.getName()).append("\",\n")
+			.append("\t\"age\": \"").append(emp.getAge()).append("\",\n")
 			.append("\t\"salary\": \"").append(emp.getSalary()).append("\"\n")
-		.append("}");
+		.append("},");
 		
 		return builder.toString();
 	}
